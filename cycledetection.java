fb5e546a8c4edc5249743eda6f1f0425b5b13d51
kurmanadhvalla <kurmanathvalla@gmail.com>
2017-09-26 22:47:30 +0530

    Add files via upload

--- cycledetection.java
+++ cycledetection.java
@@ -28,3 +28,62 @@ class qs {
     return false;
   }
 }
+
+//for undirected Graphs
+// A Java Program to detect cycle in an undirected graph
+import java.io.*;
+import java.util.*;
+ class Graph
+{
+    private int V;   // No. of vertices
+    private LinkedList<Integer> adj[]; // Adjacency List Represntation
+   Graph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for(int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+   void addEdge(int v,int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+	 Boolean isCyclic()
+    {
+        Boolean visited[] = new Boolean[V];
+        for (int i = 0; i < V; i++)
+            visited[i] = false;
+     for (int u = 0; u < V; u++)
+            if (!visited[u]) 
+                if (isCyclicUtil(u, visited, -1))
+                    return true;
+ 
+        return false;
+    }
+	Boolean isCyclicUtil(int v, Boolean visited[], int parent)
+    {
+        // Mark the current node as visited
+        visited[v] = true;
+        Integer i;
+ 
+        // Recur for all the vertices adjacent to this vertex
+        Iterator<Integer> it = adj[v].iterator();
+        while (it.hasNext())
+        {
+            i = it.next();
+ 
+            // If an adjacent is not visited, then recur for that
+            // adjacent
+            if (!visited[i])
+            {
+                if (isCyclicUtil(i, visited, v))
+                    return true;
+            }
+ 
+            // If an adjacent is visited and not parent of current
+            // vertex, then there is a cycle.
+            else if (i != parent)
+                return true;
+        }
+        return false;
+    }
+}
